0febf0cad889bc5fca0fe75e27f1131195fec8fe
Abi Shrestha <60970080+scifisatan@users.noreply.github.com>
2020-06-16 02:11:09 +0545

    Update bot.py
    Db removed.

--- bot.py
+++ bot.py
@@ -6,15 +6,13 @@ import schedule
 import tweepy
 
 #Twitter API Authentication
-""" Get your API keys from twitter developer program and paste it here"""
-consumer_key = ""
-consumer_secret = ""
-access_token = ""
-access_token_secret = ""
+consumer_key = "Z4GnZtQPUqPCdNh2fNR3mooQ6"
+consumer_secret = "xO35iLKm5XifzphrhVdvpjq7uKOgFQwF87HxQqOR0TlzzvYvBy"
+access_token = "1261594236207525888-wAzxJpiRY8Uk5pwcEzeOyzGQdXbbuf"
+access_token_secret = "A01ydtGKRCFgJamaNOrH7yp6zzExE1hKJ014qatc8VW8p"
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-
 
 # Global Variables
 url = "https://covid19.mohp.gov.np/covid/api/confirmedcases"
@@ -29,8 +27,11 @@ def stats(link):
     positive = data.get('nepal').get('positive')
     deaths = data.get('nepal').get('deaths')
     dt = data.get('nepal').get('created_at')
-    recovered = BeautifulSoup(requests.get("https://www.worldometers.info/coronavirus/country/nepal/").text,'html.parser').find_all('div',{'class':'maincounter-number'})[2].text.strip()
-    new_data = f"{tested} {negative} {positive} {deaths} {recovered} {dt}"
+    recovered = data.get('nepal').get('extra1')
+    today_death = data.get('nepal').get('today_death')
+    today_newcase = data.get('nepal').get('today_newcase')
+    today_recovered = data.get('nepal').get('today_recovered')
+    new_data = f"{tested} {negative} {positive} {deaths} {recovered} {dt} {today_death} {today_newcase} {today_recovered}"
     return new_data
 
 
@@ -45,7 +46,7 @@ def update(new_datal):
 # Formats Tweet
 def format_post(new_datal,db_data):
     msg = f"Total Positive Cases: {new_datal[2]}\nDeaths: {new_datal[3]}\nRecovered: {new_datal[4]}\nSamples Tested: {new_datal[0]}\nUpdated at: {update(new_datal)}\n"
-    new_case = int(new_datal[2])-int(db_data[2])
+    new_case = int(new_datal[2])-int(db_data[1])
     if new_case == 0:
         new_case = "no"
         case_value = 'cases'
@@ -56,8 +57,8 @@ def format_post(new_datal,db_data):
     else:
         case_value = 'cases'
         be_verb = 'have'
-        
-    new_death = int(new_datal[3])-int(db_data[3])
+
+    new_death = int(new_datal[3])-int(db_data[2])
     if new_death == 0:
         new_death = "no"
         death_value = 'deaths'
@@ -74,24 +75,56 @@ def format_post(new_datal,db_data):
         msg = f"{new_case} new COVID-19 {case_value} {be_verb} been reported by MoHP.\n\n"+msg
     else:
         msg = f"{new_case} new COVID-19 {case_value} and {new_death} new COVID-19 related {death_value} in Nepal {be_verb} been reported by MoHP.\n\n"+msg
-    return msg                  
+    print("tweet formatted")
+    return msg
 
 
 # Tweets
 def tweet(msg):
     api.update_status(msg)
+    print("tweeted")
 
 #Daily Update
 def daily_update():
-    print("Daily Update initiating...")
-    data_today = stats(url)
-    data_todayl = data_today.split(' ')
-    with open('daily_data.txt','r') as f:
-        db_data_yesterday = f.readline().strip().split(' ')
-        post = f"Updates From Last 24 Hours\n\nSamples tested: {int(data_todayl[0])-int(db_data_yesterday[0])}\nNew COVID-19 cases: {int(data_todayl[2])-int(db_data_yesterday[2])}\nPatients recovered: {int(data_todayl[4])-int(db_data_yesterday[4])}\nCOVID-19 related deaths: {int(data_todayl[3])-int(db_data_yesterday[3])}\n"
-        tweet(post)
-    with open('daily_data.txt','w') as f:
-        f.write(data_today+'\n')
+    url = "https://covid19.mohp.gov.np/covid/api/confirmedcases"
+    data = requests.get(url).json()
+    today_death = data.get('nepal').get('today_death')
+    today_newcase = data.get('nepal').get('today_newcase')
+    today_recovered = data.get('nepal').get('today_recovered')
+    post = f"Updates from last 24 Hours\n\nNew COVID-19 cases: {today_newcase}\nPatients recovered: {today_recovered}\nCOVID-19 related deaths: {today_death}\n"
+    tweet(post)
+
+
+def prev_data():
+    tweets = []
+    username = 'NepalCovid19Bot'
+
+    for tweet in api.user_timeline(id=username, count=10, tweet_mode = 'extended'):
+        tweets.append(tweet.full_text)
+
+    for t in tweets:
+        words = t.split()
+        if "reported by MoHP." in t:
+            data = words
+            break
+
+    i = 0
+    for word in data:
+        if word == 'Cases:':
+            total = data[i+1]
+        elif word == 'Deaths:':
+            deaths = data[i+1]
+        elif word == 'Recovered:':
+            recovered = data[i+1]
+        elif word == 'Tested:':
+            sample = data[i+1]
+        i +=1
+
+    data = f'{sample} {total} {deaths} {recovered}'
+    msg = data.strip().split(' ')
+    print("previous data extracted")
+    print(msg)
+    return msg
 
 # Main
 schedule.every().day.at('20:00').do(daily_update)
@@ -99,20 +132,24 @@ schedule.every().day.at('20:00').do(daily_update)
 while check:
     try:
         schedule.run_pending()
-        with open('covid19_data.txt','r') as f:
-            db_data = f.readline().strip().split(' ')
+        db_data = prev_data()
         new_data = stats(url)
         new_datal = new_data.split(' ')
-        msg = format_post(new_datal,db_data)
-        if new_datal[2] == db_data[2] and new_datal[3] == db_data[3]:
-            pass
-        else:
+        if new_datal[3] != db_data[2] or new_datal[2] != db_data[1]:
+            msg = format_post(new_datal,db_data)
             print("New data found.... Tweeting...")
-            tweet(msg)
-            with open('covid19_data.txt','w') as f:
-                f.write(new_data+'\n')
+            print(new_datal,db_data)
+            try:
+                tweet(msg)
+            except:
+                pass
+        else:
+            pass
     except KeyboardInterrupt:
         break
     except:
         pass
+
+
+
 
